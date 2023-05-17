35304357dca527f9f15d055497180d51f7d04be8
abukawsar45 <kawsar1146236@gmail.com>
2023-05-18 01:32:03 +0600

    delete method done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// kLlQv6DB84U0lQ0g
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9lqzgjv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +42,17 @@ async function run() {
       const add = req.body;
       console.log(add);
       const result = await toysCollection.insertOne(add);
+      res.send(result);
+    })
+
+    // update method
+
+    // delete method  
+    app.delete('/myToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await toysCollection.deleteOne(query);
+      console.log(result);
       res.send(result);
     })
 
