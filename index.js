321adb89008ac76bc48100bfd6c4877a860890c9
abukawsar45 <kawsar1146236@gmail.com>
2023-05-19 21:34:31 +0600

    get data specific id function done

--- index.js
+++ index.js
@@ -32,21 +32,29 @@ async function run() {
     // get method
     app.get('/allToys', async (req, res) => {
       const result = await toysCollection.find().toArray();
-      console.log(result);
+      // console.log(result);
       res.send(result);
     })
-
-    // get data use specific email 
+ 
     app.get('/myToys/:email', async (req, res) => {
       // console.log(req.params.email);
       const result = await toysCollection.find({ postBy: req.params.email }).toArray(); 
       res.send(result)
     })
 
+    app.get('/allToys/:id',async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await toysCollection.findOne(query);
+      // console.log(result);
+      res.send(result)
+
+    })
+
     // post method
     app.post('/addToys', async (req, res) => {
       const add = req.body;
-      console.log(add);
+      // console.log(add);
       const result = await toysCollection.insertOne(add);
       res.send(result);
     })
@@ -56,14 +64,14 @@ async function run() {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
       const updateToys = req.body;
-      console.log(updateToys);
+      // console.log(updateToys);
       const updateDoc = {
         $set:{
          ...updateToys
         }
       } 
       const result = await toysCollection.updateOne(filter, updateDoc)
-      console.log({result});
+      // console.log({result});
       res.send(result)
     })
 
@@ -73,7 +81,7 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await toysCollection.deleteOne(query);
-      console.log(result);
+      // console.log(result);
       res.send(result);
     })
 
@@ -94,7 +102,7 @@ run().catch(console.dir);
 
 
 
-console.log(process.env.DB_USER);
+// console.log(process.env.DB_USER);
 
 app.get('/', (req, res) => {
   res.send('amake khuje pawa gese')
